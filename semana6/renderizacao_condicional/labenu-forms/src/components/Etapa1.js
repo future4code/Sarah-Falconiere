20b8645160c32aae162cb0ee419f8638fcfdd0e6
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2021-09-13 17:26:05 -0300

    Select da Etapa 1

--- semana6/renderizacao_condicional/labenu-forms/src/components/Etapa1.js
+++ semana6/renderizacao_condicional/labenu-forms/src/components/Etapa1.js
@@ -4,13 +4,19 @@ function Etapa1() {
     <div>
       <h1>ETAPA 1 - DADOS GERAIS</h1>
       <p>1. Qual o seu nome?</p>
-      <input placeholder="ummm"/>
+      <input placeholder="ummm" />
       <p>1. Qual a sua idade?</p>
-      <input placeholder="doiss"/>
+      <input placeholder="doiss" />
       <p>1. Qual seu email?</p>
-      <input placeholder="tress"/>
+      <input placeholder="tress" />
       <p>1. Qual a sua escolaridade?</p>
-      <input placeholder="quatroo"/>
+      <select>
+      <option selected value="Ensino médio incompleto">Ensino médio incompleto</option>
+        <option value="Ensino médio completo">Ensino médio completo</option>
+        <option value="Ensino superior incompleto">Ensino superior incompleto</option>
+        
+        <option value="Ensino superior completo">Ensino superior completo</option>
+      </select>
     </div>
   );
 }
