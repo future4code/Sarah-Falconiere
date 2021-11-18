21912d7f44592ee9f688dcf4dd0f139d5f0b1bd5
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2021-11-18 18:43:09 -0300

    continuação

--- semana15/aula43/template-express/src/index.ts
+++ semana15/aula43/template-express/src/index.ts
@@ -1,10 +1,82 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { countries } from './data'
+import { country } from './types'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+//ENDPOINT DE TESTE
+app.get(
+    "/test",
+    (req, res) => {
+        res.send("Olá do Express")
+    })
+
+//ENDPOINT DE BUSCAR TODOS OS PAÍSES
+app.get("/countries", (req: Request, res: Response) => {
+
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+    }))
+
+    res
+        .status(200)
+        .send(result)
+})
+
+
+//ENDPOINT DE BUSCAR COM FILTROS
+app.get("/countries/search", (req: Request, res: Response) => {
+
+    let result: country[] = countries
+
+    if (req.query.name) {
+        result = result.filter(
+            country => country.name.includes(req.query.name as string)
+        )
+    }
+
+    if (req.query.capital) {
+        result = result.filter(
+            country => country.capital.includes(req.query.capital as string)
+        )
+    }
+
+    if (req.query.continent) {
+        result = result.filter(
+            country => country.continent.includes(req.query.continent as string)
+        )
+    }
+
+    res
+        .status(200)
+        .send(result)
+})
+
+
+
+//ENDPOINT DE BUSCAR PAÍS POR ID
+app.get("/countries/:id", (req, res) => {
+
+    const id = req.params.id
+
+    const result = countries.find(country => {
+        return country.id === Number(id)
+    })
+
+    if (result)
+        res.status(200).send(result)
+    else
+        res.status(404).send("País não encontrado")
+})
+
+
+
 
 app.listen(3003, () => {
     console.log("Servidor pronto!")
